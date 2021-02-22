986e66363da6c999a9937e6fd90391cd8b6ae11c
Sergey Glushakov <sergey_glushakov_web@mail.ru>
2021-02-22 23:17:26 +0300

    add js

---
+++ drag_drop/script_21.js
@@ -0,0 +1,26 @@
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
+
+function dragStart() {
+    console.log('drag start');
+}
+
+function dragEnd() {
+    console.log('drag end');
+}
+
+function dragOver() {
+    console.log('drag Over');
+}
+
+function dragEnter() {
+    console.log('drag Enter');
+}
+
+function dragLeave() {
+    console.log('drag Leave');
+}
+
+function dragDrop() {
+    console.log('drag Drop');
+}
